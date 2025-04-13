a2d1a5df93b817ecaab44bf74e3a01dc86a4cfcf
Drew <tullos.andrew@gmail.com>
2025-04-13 00:59:27 -0500

    Landing Page Bug Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import LandingPage from "./landing/page"
+import NoiseGradient from "@/components/NoiseGradient";
 
 export default function Home() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <>
+      <div style={{ position: "relative", minHeight: "100vh" }}>
+        <NoiseGradient />
+        <LandingPage />
+      </div>
+    </>
   );
 }
